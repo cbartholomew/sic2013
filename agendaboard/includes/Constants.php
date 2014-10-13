4f697b595a87d149d6507d2c713690d420a9b497
Christopher Bartholomew <cbartholomew@gmail.com>
2014-10-12 20:09:53 -0400

    updated agenda location
    I’ve updated the agenda location to point to the new agenda
    configuration.

--- agendaboard/includes/Constants.php
+++ agendaboard/includes/Constants.php
@@ -6,10 +6,10 @@
 
 	// front end agenda board related constants
 	define("JSON_SESSION_PROD_URI","http://2014.seattleinteractive.com/site/JSON");
-	define("JSON_AGENDA_PROD_URI", "http://seattleinteractive.com/agendaboard/Configuration/Agenda.json");
+	define("JSON_AGENDA_PROD_URI", "http://2014-agenda.seattleinteractive.com/Configuration/Agenda.json");
 	define("JSON_SESSION_TEST_URI","http://localhost/aoit/sic2013/agendaboard/Configuration/Sessions.json");
 	define("JSON_AGENDA_URI", "http://localhost/aoit/sic2013/agendaboard/Configuration/Agenda.json");
-	define("IS_PROD", true);
+	define("IS_PROD", false);
 	define("DISABLE_TWEETS", true);
 
 
